e78499d2bd3ddbbdfe29c50c7c92b115e56e2c8c
Ken Yoshizoe <63041076+kenyoshizoe@users.noreply.github.com>
2023-02-26 20:58:03 +0900

    add winget.ps1

---
+++ winget.ps1
@@ -0,0 +1,40 @@
+# Driver
+winget install -e --id Nvidia.GeForceExperience
+winget install -e --id Logitech.GHUB
+winget install -e --id MOTU.MSeries
+# Core
+winget install -e --id Microsoft.PowerToys
+winget install -e --id Google.JapaneseIME
+winget install -e --id Google.Chrome
+winget install -e --id Google.ChromeRemoteDesktop
+winget install -e --id 7zip.7zip
+# SNS
+winget install -e --id SlackTechnologies.Slack
+winget install -e --id Discord.Discord
+winget install -e --id Zoom.Zoom
+# Develop
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id Microsoft.Office
+winget install -e --id TeraTermProject.teraterm
+winget install -e --id Docker.DockerDesktop
+winget install -e --id Git.Git
+winget install -e --id KiCad.KiCad
+winget install -e --id Prusa3D.PrusaSlicer
+winget install -e --id ArduinoSA.IDE.stable
+# Media
+winget install -e --id Typora.Typora
+winget install -e --id Audacity.Audacity
+winget install -e --id GIMP.GIMP
+winget install -e --id OBSProject.OBSStudio
+winget install -e --id Unity.UnityHub
+# Player
+winget install iTunes -s msstore
+winget install -e --id VideoLAN.VLC
+# Game
+winget install -e --id Valve.Steam
+winget install -e --id RiotGames.Valorant.AP
+winget install -e --id Mojang.MinecraftLauncher
+
+# Didn't work
+# winget install -e --id Apple.iTunes
